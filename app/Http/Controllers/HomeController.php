4cb99215607d3c4e244db974d57499041be46092
Gwion Tangi <lil.smiss7@gmail.com>
2023-01-10 16:58:49 +0100

    displayed my name

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class HomeController extends Controller
 {
     public function homePage(){
         //Find User
         $user = User::find(1);
-        return view('Welcome', $user);
+        return view('welcome', ['user' => $user]);
     }
 }
